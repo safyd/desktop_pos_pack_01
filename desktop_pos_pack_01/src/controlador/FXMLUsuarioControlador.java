3c70e0612c9c2eeab1152c3cb3e8cd88be4c8424
JesusHilario <33611574+JesusHilario@users.noreply.github.com>
2018-04-30 01:08:07 -0500

    Codigo Secundario

--- desktop_pos_pack_01/src/controlador/FXMLUsuarioControlador.java
+++ desktop_pos_pack_01/src/controlador/FXMLUsuarioControlador.java
@@ -1,8 +1,20 @@
 package controlador;
 
+import entidad.usuario;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -13,10 +25,14 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafxpixel.JavaFXPixel;
+import javax.imageio.ImageIO;
 import utils.uri;
 import utils.utils;
 
@@ -27,7 +43,7 @@ public class FXMLUsuarioControlador extends Application implements Initializable
     @FXML
     private ImageView imgImagen;
     @FXML
-    private ComboBox<?> cbxSexo;
+    private ComboBox<String> cbxSexo;
     @FXML
     private PasswordField txtContrasena, txtConfirmarContrasena;
     @FXML
@@ -51,7 +67,12 @@ public class FXMLUsuarioControlador extends Application implements Initializable
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.txtPerfil.setEditable(false);
+        this.cbxSexo.getItems().addAll(
+                "Hombre",
+                "Mujer"
+        );
         this.eventoBoton();
+
     }
 
     private Boolean validarCampos() {
@@ -65,11 +86,35 @@ public class FXMLUsuarioControlador extends Application implements Initializable
 
     private void guardarUsuario() {
         if (this.validarCampos()) {
+            usuario usu = new usuario();
+            //usu.setPer_id();
+            //usu.setUsu_imagen();
+            usu.setUsu_usuario(this.txtUsuario.getText());
+            usu.setUsu_contraseña(utils.Encriptar(this.txtContrasena.getText()));
+            usu.setUsu_nombre(this.txtNombre.getText());
+            usu.setUsu_apellido(this.txtApellido.getText());
+            usu.setUsu_sexo(this.cbxSexo.getSelectionModel().getSelectedItem());
+            if (usu.insert()) {
+                utils.mensaje("Registro exitoso.", "El usuario ha sido registrado con exito.", Alert.AlertType.ERROR);
+            } else {
+                utils.mensaje("Error de registro.", "El usuario no se registro correctamente.", Alert.AlertType.ERROR);
+            }
         } else {
             utils.mensaje("Faltan campos de llenar.", "Es necesario llenar los campos con asterisco", Alert.AlertType.ERROR);
         }
     }
 
+//    public byte[] convertirImagen(String urlImagen) {
+//        try {
+//            File fnew = new File(urlImagen);
+//            BufferedImage originalImage = ImageIO.read(fnew);
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            ImageIO.write(originalImage, "jpg", baos);
+//            return baos.toByteArray();
+//        } catch (IOException ex) {
+//            Logger.getLogger(FXMLUsuarioControlador.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
     private void eventoBoton() {
         this.btnGuardar.setOnMouseClicked((event) -> {
             if (event.getClickCount() == 1) {
@@ -87,7 +132,32 @@ public class FXMLUsuarioControlador extends Application implements Initializable
         });
         this.btnCancelar.setOnMouseClicked((event) -> {
         });
+        this.btnImagen.setOnMouseClicked((event) -> {
+            this.obtenerImagen();
+        });
+        this.btnImagen.setOnKeyPressed((event) -> {
+            if (event.getCode() == KeyCode.ENTER) {
+                this.obtenerImagen();
+            } else {
+                event.consume();
+            }
+        });
 
+    }
+
+    protected void obtenerImagen() {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+        FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+        fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
+        File file = fileChooser.showOpenDialog(null);
+        try {
+            BufferedImage bufferedImage = ImageIO.read(file);
+            Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+            this.imgImagen.setImage(image);
+        } catch (IOException ex) {
+            Logger.getLogger(JavaFXPixel.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
--- desktop_pos_pack_01/src/entidad/usuario.java
+++ desktop_pos_pack_01/src/entidad/usuario.java
@@ -6,22 +6,31 @@ public class usuario extends ORM<usuario> {
 
     private Long usu_id;
     private Integer per_id;
-    private byte[] art_imagen;
+    private byte[] usu_imagen;
     private String usu_usuario;
     private String usu_contraseña;
     private String usu_nombre;
     private String usu_apellido;
+    private String usu_sexo;
+
+    public byte[] getUsu_imagen() {
+        return usu_imagen;
+    }
+
+    public void setUsu_imagen(byte[] usu_imagen) {
+        this.usu_imagen = usu_imagen;
+    }
 
     public usuario() {
         super.setClass(this);
     }
 
-    public byte[] getArt_imagen() {
-        return art_imagen;
+    public String getUsu_sexo() {
+        return usu_sexo;
     }
 
-    public void setArt_imagen(byte[] art_imagen) {
-        this.art_imagen = art_imagen;
+    public void setUsu_sexo(String usu_sexo) {
+        this.usu_sexo = usu_sexo;
     }
 
     public Long getUsu_id() {
