ed91a5dcc1ac843865116e8e0e64cc3ab3b42cac
JesusHilario <33611574+JesusHilario@users.noreply.github.com>
2018-05-04 23:00:13 -0500

    Codigo Secundario

---
+++ desktop_pos_pack_01/src/entidad/Perfil.java
@@ -0,0 +1,57 @@
+package entidad;
+
+import Safyd.System.DataBase.ORM;
+
+public class Perfil extends ORM<Perfil> {
+
+    private Integer per_id;
+    private String per_clave;
+    private String per_descripcion;
+
+    public Perfil() {
+        super.setClass(this);
+    }
+
+    public Perfil(
+            Integer per_id,
+            String per_clave,
+            String per_descripcion
+    ) {
+
+        super.setClass(this);
+        this.per_id = per_id;
+        this.per_clave = per_clave;
+        this.per_descripcion = per_descripcion;
+
+    }
+
+    public Integer getPer_id() {
+        return per_id;
+    }
+
+    public void setPer_id(Integer per_id) {
+        this.per_id = per_id;
+    }
+
+    public String getPer_clave() {
+        return per_clave;
+    }
+
+    public void setPer_clave(String per_clave) {
+        this.per_clave = per_clave;
+    }
+
+    public String getPer_descripcion() {
+        return per_descripcion;
+    }
+
+    public void setPer_descripcion(String per_descripcion) {
+        this.per_descripcion = per_descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return this.per_clave;
+    }
+
+}
